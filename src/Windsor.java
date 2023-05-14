0a68e2b2738abb28fab6d5fdc9b99510cf80912d
KnudNielsen999 <jervelund.nielsen@hotmail.com>
2023-05-14 21:17:21 +0200

    dette er en test222

---
+++ src/Windsor.java
@@ -0,0 +1,23 @@
+import Models.LagerModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Windsor {
+    public static void main(String[] args) {
+        List<LagerModel> lagerListe = new ArrayList<>();
+
+        System.out.println("Windsor ERP System\n");
+
+        LagerModel lm1 = new LagerModel();
+        lm1.setVarenummer("1");
+        lm1.setKostPris(10.5);
+        lagerListe.add(lm1);
+
+        for (LagerModel lm : lagerListe) {
+            System.out.println(lm);
+        }
+        System.out.println("\n");
+        lm1.varenummerVisning();
+    }
+}
